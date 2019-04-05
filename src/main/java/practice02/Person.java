9aa1400be63d67bedb29b3340d87b77e9fbc5875
fishsalted <10572677+fishsalted@users.noreply.github.com>
2019-04-05 12:40:08 +0800

    完成测试2

--- src/main/java/practice02/Person.java
+++ src/main/java/practice02/Person.java
@@ -1,4 +1,24 @@
-package practice02;
-
-public class Person {
-}
+//package practice02;
+//
+//public class Person {
+//
+//    private int age_;
+//    private String name_;
+//
+//    public Person(String name, int age) {
+//        age_ = age;
+//        name_ = name;
+//    }
+//
+//    public String getName() {
+//        return name_;
+//    }
+//
+//    public int getAge() {
+//        return age_;
+//    }
+//
+//    public String introduce() {
+//        return "My name is " + name_ + ". I am " + age_ + " years old.";
+//    }
+//}
--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,20 @@
 package practice02;
 
-public class Student {
+public class Student extends practice01.Person {
+
+    int klass_;
+
+    public Student(String name, int age, int klass) {
+        super(name,age);
+        klass_ = klass;
+    }
+
+    public int getKlass() {
+        return klass_;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class " + klass_ + ".";
+    }
+
 }
